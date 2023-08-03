86534a201c64c09ebb89966b9f854448e3ffe671
muhamadariq-09 <muhamadariq77@gmail.com>
2023-08-04 01:43:31 +0700

    update location request

--- app/src/main/java/com/example/mytracking/activity/home/HomeFragment.kt
+++ app/src/main/java/com/example/mytracking/activity/home/HomeFragment.kt
@@ -34,6 +34,7 @@ import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.location.LocationSettingsRequest
+import com.google.android.gms.location.Priority
 import com.google.android.gms.location.SettingsClient
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -287,12 +288,12 @@ class HomeFragment : Fragment(), OnMapReadyCallback {
         }
 
 
-    @Suppress("DEPRECATION")
+
     private fun createLocationRequest() {
         locationRequest = LocationRequest.create().apply {
             interval = TimeUnit.SECONDS.toMillis(1)
             maxWaitTime = TimeUnit.SECONDS.toMillis(1)
-            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+            priority = Priority.PRIORITY_HIGH_ACCURACY
         }
 
         val builder = LocationSettingsRequest.Builder()
